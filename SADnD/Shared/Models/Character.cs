34d1bbdb1b0d7886df9ac08d12939ab662b5bd36
Ben <benjaminpeter@outlook.com>
2024-11-03 11:51:00 +0100

    Merge

--- SADnD/Shared/Models/Character.cs
+++ SADnD/Shared/Models/Character.cs
@@ -16,18 +16,18 @@ namespace SADnD.Shared.Models
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Rasse muss gewählt werden.")]
         public int RaceId { get; set; }
-        public Race? Race { get; set; }
+        public Race Race { get; set; }
         public ICollection<Models.Class>? Classes { get; set; }
         public int Hitpoints { get; set; }
         public int TemporaryHitpoints { get; set; }
-        public bool[] DeathRoles { get; set; }
-        public string Background {  get; set; }
-        public int Age { get; set; }
-        public char SizeCategory { get; set; }
-        public float Size {  get; set; }
-        public float Weight { get; set; }
-        public string Sex { get; set; }
-        public string Alignment { get; set; }
+        public bool[]? DeathRoles { get; set; }
+        public string? Background {  get; set; }
+        public int? Age { get; set; }
+        public char? SizeCategory { get; set; }
+        public float? Size {  get; set; }
+        public float? Weight { get; set; }
+        public string? Sex { get; set; }
+        public string? Alignment { get; set; }
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
         public string? CampaignId { get; set; }
