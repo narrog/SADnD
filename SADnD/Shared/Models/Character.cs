34e7a6e969c5053579ceb22542516b7ce38e97a0
leonieb1 <me.leoniebuser.ch@zbw-online.ch>
2024-11-03 11:37:34 +0100

    Added Pages for characters

--- SADnD/Shared/Models/Character.cs
+++ SADnD/Shared/Models/Character.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,19 @@ namespace SADnD.Shared.Models
     public class Character
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Name darf max. 50 Zeichen lang sein")]
         public string Name { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Rasse muss gewählt werden.")]
         public int RaceId { get; set; }
-        public Race Race { get; set; }
-        public ICollection<Models.Class> Classes { get; set; }
+        public Race? Race { get; set; }
+        public ICollection<Models.Class>? Classes { get; set; }
         public int Hitpoints { get; set; }
         public int TemporaryHitpoints { get; set; }
-        public bool[] DeathRoles { get; set; }
-        public string UserId { get; set; }
-        public ApplicationUser User { get; set; }
+        public bool[]? DeathRoles { get; set; }
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
         public string? CampaignId { get; set; }
         public Campaign? Campaign { get; set; }
     }
