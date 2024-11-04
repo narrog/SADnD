c7a3bcb29d78b95c7ca552f75e6800337cc10dc3
leonieb1 <me.leoniebuser.ch@zbw-online.ch>
2024-11-04 18:02:57 +0100

    Characters - Max Lvl 20

--- SADnD/Shared/Models/CharacterClass.cs
+++ SADnD/Shared/Models/CharacterClass.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,13 @@ namespace SADnD.Shared.Models
     public class CharacterClass
     {
         public int Id { get; set; }
+        [Range (0, 20, ErrorMessage = "Maximum Level: 20")]
         public int Level { get; set; }
         public int CharacterId { get; set; }
         [JsonIgnore] 
         public Character? Character { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Klasse muss gewählt werden.")]
         public int ClassId { get; set; }
         public Class? Class { get; set; }
     }
