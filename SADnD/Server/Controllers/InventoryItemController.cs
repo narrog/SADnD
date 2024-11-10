aede1a31d94db10b4c889dd044c2d9130ab00e9e
Ben <benjaminpeter@outlook.com>
2024-11-10 15:13:36 +0100

    added Item controller

---
+++ SADnD/Server/Controllers/InventoryItemController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SADnD.Server.Areas.Identity;
+using SADnD.Server.Data;
+using SADnD.Shared;
+using SADnD.Shared.Models;
+using System.Security.Claims;
+
+namespace SADnD.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryItemController : ControllerBase
+    {
+        UserManager<ApplicationUser> _userManager;
+        EFRepositoryGeneric<InventoryItem, ApplicationDbContext> _itemManager;
+        public InventoryItemController(
+            UserManager<ApplicationUser> userManager, 
+            EFRepositoryGeneric<InventoryItem, ApplicationDbContext> itemManager)
+        {
+            _userManager = userManager;
+            _itemManager = itemManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<APIListOfEntityResponse<InventoryItem>>> GetAllItems()
+        {
+            try
+            {
+                var campaignClaims = User.Claims.Where(c => c.Type == "Campaign").ToList();
+                var result = new List<InventoryItem>();
+                foreach (var claim in campaignClaims)
+                {
+                    result.AddRange(await _itemManager.Get(i => i.CampaignId == claim.Value));
+                }
+                return Ok(new APIListOfEntityResponse<InventoryItem>()
+                {
+                    Success = true,
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                // TODO: log Exception
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<APIEntityResponse<InventoryItem>>> GetByInventoryItemId(int id)
+        {
+            try
+            {
+                var result = (await _itemManager.GetByID(id));
+                if (result != null)
+                {
+                    return Ok(new APIEntityResponse<InventoryItem>()
+                    {
+                        Success = true,
+                        Data = result
+                    });
+                }
+                else
+                {
+                    return Ok(new APIEntityResponse<InventoryItem>()
+                    {
+                        Success = false,
+                        ErrorMessages = new List<string>() { "InventoryItem Not Found" },
+                        Data = null
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: log Exception
+                return StatusCode(500);
+            }
+        }
+
+        //[HttpPost]
+        //public async Task<ActionResult<APIEntityResponse<InventoryItem>>> Post([FromBody] InventoryItem race)
+        //{
+        //    try
+        //    {
+        //        await _itemManager.Insert(race);
+        //        var result = (await _itemManager.Get(x => x.Id == race.Id)).FirstOrDefault();
+        //        if (result != null)
+        //        {
+        //            return Ok(new APIEntityResponse<InventoryItem>()
+        //            {
+        //                Success = true,
+        //                Data = result
+        //            });
+        //        }
+        //        else
+        //        {
+        //            return Ok(new APIEntityResponse<InventoryItem>()
+        //            {
+        //                Success = false,
+        //                ErrorMessages = new List<string>() { "Could not find race after adding it" },
+        //                Data = null
+        //            });
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // TODO: log Exception
+        //        return StatusCode(500);
+        //    }
+        //}
+
+        //[HttpPut]
+        //public async Task<ActionResult<APIEntityResponse<InventoryItem>>> Put([FromBody] InventoryItem race)
+        //{
+        //    try
+        //    {
+        //        await _itemManager.Update(race);
+        //        var result = (await _itemManager.Get(x => x.Id == race.Id)).FirstOrDefault();
+        //        if (result != null)
+        //        {
+        //            return Ok(new APIEntityResponse<InventoryItem>()
+        //            {
+        //                Success = true,
+        //                Data = result
+        //            });
+        //        }
+        //        else
+        //        {
+        //            return Ok(new APIEntityResponse<InventoryItem>()
+        //            {
+        //                Success = false,
+        //                ErrorMessages = new List<string>() { "Could not find race after updating it" },
+        //                Data = null
+        //            });
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // TODO: log Exception
+        //        return StatusCode(500);
+        //    }
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult> Delete(int id)
+        //{
+        //    try
+        //    {
+        //        if (await _itemManager.Get(x => x.Id == id) != null)
+        //        {
+        //            var success = await _itemManager.Delete(id);
+        //            if (success)
+        //            {
+        //                return NoContent();
+        //            }
+        //            else
+        //            {
+        //                return StatusCode(500);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return StatusCode(500);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // TODO: log Exception
+        //        return StatusCode(500);
+        //    }
+        //}
+    }
+}
