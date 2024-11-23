d324d9ed15b10ec81d9114cc4fdbcb4bbc70b4ed
Ben <benjaminpeter@outlook.com>
2024-11-23 16:39:51 +0100

    Install Button for PWA created

---
+++ SADnD/Client/wwwroot/js/installPWAprompt.js
@@ -0,0 +1,27 @@
+﻿window.addEventListener('beforeinstallprompt', function (e) {
+    e.preventDefault();
+    window.PWADeferredPrompt = e;
+
+    // Nach ca 10min durchgehender Benutzung soll die Frage für die Installation angezeigt werden.
+    setTimeout(() => {
+        BlazorPWA.ComponentReference.invokeMethodAsync("ActivateInstallButton")
+            .catch(err => console.error("Error invoking Blazor method:", err));
+    }, 600000);
+});
+
+window.BlazorPWA = {
+    ComponentReference: null,
+    StoreComponent: function (componentReference) {
+        BlazorPWA.ComponentReference = componentReference;
+    },
+    InstallPWA: function () {
+        if (window.PWADeferredPrompt) {
+            window.PWADeferredPrompt.prompt();
+            window.PWADeferredPrompt.userChoice
+                .then(function (choiceResult) {
+                    BlazorPWA.ComponentReference.invokeMethodAsync("ChoiceResult", choiceResult.outcome);
+                    window.PWADeferredPrompt = null;
+                });
+        }
+    }
+};
