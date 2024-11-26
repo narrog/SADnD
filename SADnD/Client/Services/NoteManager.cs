4318eb15f15da3894c72320003e494476361132b
Ben <benjaminpeter@outlook.com>
2024-11-26 17:23:23 +0100

    Added Service for NoteManager

---
+++ SADnD/Client/Services/NoteManager.cs
@@ -0,0 +1,13 @@
+﻿using SADnD.Client.Shared;
+using SADnD.Shared.Models;
+
+namespace SADnD.Client.Services
+{
+    public class NoteManager : DataManager<Note>
+    {
+        public NoteManager(NoteApiManager apiManager)
+            : base(apiManager)
+        {
+        }
+    }
+}
