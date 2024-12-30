dff5d83bdb743dc9b7aca09d2e0d846da9d2a79e
Ben <benjaminpeter@outlook.com>
2024-12-30 14:45:48 +0100

    Added a page to synchronise indexeddb after login

--- SADnD/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SADnD/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -103,7 +103,7 @@ namespace SADnD.Server.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl = Url.Content("~/LoggedIn");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
